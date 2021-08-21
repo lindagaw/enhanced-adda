ef2879096d3407aef2fb2588969916ca9bf27f9c
Ashley Gao <gaoye0718@gmail.com>
2021-08-21 16:06:58 -0400

    WIP

--- datasets/office_31.py
+++ datasets/office_31.py
@@ -112,10 +112,10 @@ class Office_31(data.Dataset):
 def get_office_31(train, dataset):
 
     pre_process = transforms.Compose([
-        transforms.Scale((256, 256)),
+        transforms.Resize((256, 256)),
         transforms.RandomCrop((224, 224)),
         transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
+        #transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
